99a5e418e2e69c14814c44d1d042ffa6792d0534
Mike Stefanuk <mike.stefanuk@gmail.com>
2018-04-13 14:02:22 -0400

    add min and max

--- summarizer.r
+++ summarizer.r
@@ -1,7 +1,7 @@
 #' summarizer
 #' 
 #' Gives summary statistics of inputted Daymet data.
-#' Includes: mean, median & sd
+#' Includes: min, mean, median, sd & max
 #' 
 #' @param data An R object containing the data to be summarized
 
@@ -20,15 +20,17 @@ summarizer <- function(data = NULL){
   c_names <- colnames(data, do.NULL = FALSE)
   
   # calculate summary stats
+  s_min <- sapply(data, FUN = min, na.rm = TRUE)
   s_mean <- sapply(data, FUN = mean, na.rm = TRUE)
   s_median <- sapply(data, FUN = median, na.rm = TRUE)
   s_sd <- sapply(data, FUN = sd, na.rm = TRUE)
+  s_max <- sapply(data, FUN = max, na.rm = TRUE)
   
   # create output dataframe
-  result <- rbind(s_mean, s_median, s_sd)
+  result <- rbind(s_min, s_mean, s_median, s_sd, s_max)
   
   # define row names
-  row.names(result) <- c("mean","median","sd")
+  row.names(result) <- c("min","mean","median","sd","max")
   
   # return result
   return(result)
