ba1fe56e5ea8710baf443ad93ae2e42baccf8f6c
Mike Stefanuk <mike.stefanuk@gmail.com>
2018-05-09 12:57:06 -0400

    trending update

--- trending.r
+++ trending.r
@@ -5,18 +5,23 @@
 #' only significant results at a user-specified
 #' threshold.
 #' @param file An r object or a character file name
+#' @param change Logical, if TRUE the slope will be multiplied by the number of bands
 #' @param threshold Significance threshold (e.g. 0.05)
 #'
 #'https://matinbrandt.wordpress.com/2013/11/15/pixel-wise-time-series-trend-anaylsis-with-ndvi-gimms-and-r/
 
-trending <- function(file = NULL, threshold = NULL){
+trending <- function(file = NULL, change = FALSE, threshold = NULL){
   # input check
   if(is.null(file)){
     stop('Please specify the data source')
   }
   
   # load data
-  dat <- suppressWarnings(raster::brick(file))
+  if(class(file) != "RasterBrick"){
+    dat <- suppressWarnings(raster::brick(file, values = TRUE))
+  } else {
+    dat <- file
+  }
   
   # create time variable
   time <- 1:raster::nlayers(dat)
@@ -26,6 +31,11 @@ trending <- function(file = NULL, threshold = NULL){
     else {lm(x ~ time)$coefficients[2] }}
   trend <- raster::calc(dat, fun_t)
   
+  # calculate change over time
+  if(change == TRUE){
+    trend <- trend * nlayers(dat)
+  }
+  
   # if threshold unspecified, return trend
   if(is.null(threshold)){    
     return(trend)
@@ -34,13 +44,13 @@ trending <- function(file = NULL, threshold = NULL){
   if(!is.null(threshold)){
     # calculate p-values
     fun_p <- function(x) { if (is.na(x[1])){ NA } 
-      else {lm(x ~ time)$coefficients[8] }}
+      else {m = lm(x ~ time); summary(m)$coefficients[8] }}
     p_val <- raster::calc(dat, fun_p)
     
     # reclassify p-values at desired threshold
     m <- c(0, threshold, 1, threshold, 1, 0)
     rclmat <- matrix(m, ncol = 3, byrow = TRUE)
-    p_mask <- raster::reclassify(p, rclmat)
+    p_mask <- raster::reclassify(p_val, rclmat)
     fun <- function(x) { x[x<1] <- NA; return(x) }
     p_mask_na <- raster::calc(p_mask, fun)
     trend_sig <- raster::mask(trend, p_mask_na)
@@ -48,4 +58,5 @@ trending <- function(file = NULL, threshold = NULL){
     # return significant trends
     return(trend_sig)
   }
-}
+  }
+  
