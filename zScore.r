6e742532b6e384453cf2e6c11d2ce6f11cc04b7d
Mike Stefanuk <mike.stefanuk@gmail.com>
2018-07-19 15:58:06 -0400

    trouble-shot

--- zScore.r
+++ zScore.r
@@ -11,82 +11,89 @@
 #' Rodriguez-Galiano calculated z scores excluding the targeted year in 
 #' the multi-year mean to enhance inter-annual variation.
 #' 
-#' @param file A path to file containing stacked phenology data.
-#' @param ncores numeric. Number of cores to be used for processing
+#' @param file A path to file containing stacked phenology data
+#' @param na.rm Should NA values be retained or removed? (logical, default = TRUE)
+#' @param parallel Should parallel processing be used
+#'  to increase processing speed? (logical, default = TRUE)
+#' @param ncores Number of cores to be used for processing 
+#' (ignored if parallel = FALSE, numeric, default = 2)
+#' 
+#' Note that parallel processing is curreltly only functional for Windows.
+#' This can be modified to work on MacOS or Unix by modifying the cluster type.
+#' 
+#' Note that the parallel processing has been very buggy and may hang.
 
-zScore <- function(file=NULL,ncores=1){
-  
-  # set up for multi-core processing
-  beginCluster(ncores,type='SOCK')
+zScore <- function(file=NULL, na.rm = TRUE, parallel = TRUE, ncores=2){
   
   # stop if no data is provided
   if(is.null(file)){
     stop('Please specify a file path.')
   }
   
+  # begin multi-core processing if parallel = TRUE
+  if(parallel == TRUE){
+    raster::beginCluster(ncores, type='SOCK')
+    cat(sprintf('\nexecuting zScore using a cluster size of %s', ncores))
+  } else {
+    cat('\n executing zScore without parallel processing')
+  }
+  
   # load data
   data <- suppressWarnings(raster::brick(file))
   
-  ## create output raster object using characteristics of input file
-  #result <- raster::brick(crs = raster::crs(data),
-  #                        nl = raster::nbands(data))
-  #raster::extent(result) <- raster::extent(data)
-  
-  # calculate the sd
-  f1 <- function(data) { raster::calc(x = data, fun = sd, na.rm = TRUE) }
-  sd <- raster::clusterR(data, fun = f1)
-  
-  #sd <- raster::calc(data,
-  #                   fun = sd,
-  #                   na.rm = TRUE)
-  
-  f1 <- function(data) { raster::calc(x = data, fun = sd, na.rm = TRUE) }
-  sd <- raster::clusterR(data, fun = f1)
-  
-  # calculate Z-score for each layer in data
-  for (i in 1:raster::nbands(data)){
-    
-    # calculate the mean of all years excluding i
-    #mn <- raster::calc(x = data[[-i]], # should this just be [-i] or !i?
-    #                   fun = mean,
-    #                   na.rm = TRUE)
-    mn <- raster::calc(x = data[[-i]], # should this just be [-i] or !i?
-                       fun = mean,
-                       na.rm = TRUE)
-    
-    f2 <- function(data){ raster::calc(x = data[!i], fun = mean, na.rm = TRUE) }
-    mn[[i]] <- raster::clusterR(x = data, fun = f3)
+  # calculate SD of stack
+  cat('\n calculating standard deviation')
+  if(parallel == TRUE){
+    sd <- raster::clusterR(x = data, fun = function(data){
+               raster::calc(data, fun = sd, na.rm = na.rm)})
+  } else {
+    sd <- raster::calc(data, fun = sd, na.rm = na.rm)
   }
-  # calculate the z score - which one works?
-  #z <- raster::calc(x = data,
-  #                  fun = function(x) x[[i]] - mn[[i]],
-  #                  na.rm = TRUE)
-  f3 <- function(data, mn) { data - mn }
-  z <- raster::clusterR(x = data, fun = raster::overlay,
-                        args = list(x = data, y = mn,
-                                    fun = f3, na.rm = TRUE))
   
-  #z <- raster::stackApply(x = data,
-  #                        indices = ind,
-  #                        fun = function(x) x - mn,
-  #                        na.rm = TRUE)
+  # calculate mean for each layer (mean of all but band i)
+  set <- 1:raster::nbands(data)
+  mn <- data
+  for (i in set){
+    cat(sprintf('\n calculating mean for band %s', i))
+    subset <- set[-i]
+    wrk <- raster::stack(x = data, bands = subset)
+    if(parallel == TRUE){
+      mn_i <- raster::clusterR(x = wrk, fun = function(wrk){
+        raster::calc(x = wrk, fun = mean, na.rm = na.rm)})
+    } else {
+      mn_i <- raster::calc(x = wrk, fun = mean, na.rm = na.rm)
+    }
+    mn[[i]] <- mn_i
+  }
+  rm(mn_i, wrk, i, set, subset)
+    
+  # calculate the z score
+  cat('calculating z scores')
+  if(parallel == TRUE){
+    z <- raster::clusteR(x = data, fun = function(data, mn){
+      raster::overlay(x = data, y = mn, fun = function(data, mn){
+        data - mn }, na.rm = na.rm)
+    })
+  } else {
+    z <- raster::overlay(x = data, y = mn, fun = function(data, mn){
+      data - mn}, na.rm = na.rm)
+  }
   
   # normalize the z score
-  #z_n <- raster::calc(x = z,
-  #                    fun = function(x) {z[[i]] / sd},
-  #                    na.rm = TRUE)
-  f4 <- function(z, sd) { z / sd }
-  z <- raster::clusterR(x = z, fun = raster::overlay,
-                        args = list(x = z, y = sd,
-                                    fun = f4, na.rm = TRUE))
-  
-  ## write the z score to the appropriate band in result
-  #result[[i]] <- z_n
-  #}
-  
-  ##return result
-  #return(result)
+  cat('normalizing z scores')
+  if(parallel == TRUE){
+    z <- raster::clusterR(x = z, fun = function(z, sd){
+      raster::overlay(x = z, y = sd, fun = function(x, y){
+        z / sd }, na.rm = na.rm)
+    })
+  } else {
+    z <- raster::overlay(x = z, y = sd, fun = function(z, sd){
+      z / sd }, na.rm = na.rm)
+  }
   
   # end multi-core processing
-  endCluster()
+  raster::endCluster()
+  
+  #return result
+  return(z)
 }
