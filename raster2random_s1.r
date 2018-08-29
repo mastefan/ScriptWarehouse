bff81b07a93682165c16e7dcdc6d50312697d7cd
Mike Stefanuk <mike.stefanuk@gmail.com>
2018-08-28 23:26:43 -0400

    working

--- raster2random_s1.r
+++ raster2random_s1.r
@@ -3,11 +3,15 @@
 #' Takes a named list of raster bricks and converts it to a long-format 
 #' dataframe for use with the randomForest function
 #' 
-#' @param in.data Named list of input rasters. The independent variable
-#'  should be first on the list.
-#' @param na.rm logical Should rows where independent data = NA be removed?
+#' Stage 1: Creates a dataframe of a given climate variable
+#' 
+#' @param in.data Named list of input rasters. Can handle annual or 
+#' monthly data.
+#' 
+#' Note that band naming is done automatically, and can account for data 
+#' where orbital drift years have been removed
 
-raster2random <- function(in.data = NULL, na.rm = FALSE){
+raster2random_s1 <- function(in.data = NULL){
   
   # check that the inputs are a list
   if(class(in.data) != "list"){
@@ -16,14 +20,29 @@ raster2random <- function(in.data = NULL, na.rm = FALSE){
   
   # name bands of input rasters
   for(i in 1:length(in.data)){
+    # annual
     if(raster::nlayers(in.data[[i]]) == 26){
       dates <- seq(from = 1989, to = 2014, by = 1)
       names(in.data[[i]]) <- dates
     }
+    # annual no drift
+    if(raster::nlayers(in.data[[i]]) == 21){
+        dates <- seq(from = 1989, to = 2014, by = 1)
+        nd_p_band <- c(1:3, 7:10, 13:26)
+        dates <- dates[nd_p_band]
+        names(in.data[[i]]) <- dates
+      }
+    # monthly
     if(raster::nlayers(in.data[[i]]) == 312){
       dates <- zoo::as.yearmon(seq(from = as.Date("1989-01-15"),
                                    to = as.Date("2014-12-15"),
                                    by = "month"))
+      names(in.data[[i]]) <- dates
+    }
+    # monthly no drift
+    if(raster::nlayers(in.data[[i]]) == 264){
+      nd_name <- c(1988:1991, 1995:1998, 2001:2014)
+      dates <- paste(month.abb, rep(nd_name, each = 12), sep = ".")
       names(in.data[[i]]) <- dates
     }
   }
@@ -34,17 +53,19 @@ raster2random <- function(in.data = NULL, na.rm = FALSE){
   ## loop through items on list
   for(i in 1:length(in.data)){
     # annual data
-    if(raster::nlayers(in.data[[i]]) == 26){
+    if(raster::nlayers(in.data[[i]]) < 28){
       o <- raster::as.data.frame(in.data[[i]])
       o <- data.frame(pix = 1:nrow(o), o)
       o <- reshape2::melt(o, id = "pix")
-      colnames(o) <- c("pix","time",names(in.data[i]))
-      out[i] <- list(o)
+      #colnames(o) <- c("pix","time",names(in.data[i]))
+      #out[i] <- list(o)
+      out[i] <- list(o$value)
     }
     # monthly data
-    if(raster::nlayers(in.data[[i]]) == 312){
+    if(raster::nlayers(in.data[[i]]) > 28){
       o <- raster::as.data.frame(in.data[[i]])
-      colnames(o) <- paste(names(in.data[i]), rep(month.abb, 26), sep = ".")
+      colnames(o) <- paste(names(in.data[i]), 
+                           rep(month.abb, (raster::nlayers(in.data[[i]])/12), sep = "."))
       o <- sapply(unique(names(o)), function(x) unname(unlist(o[,names(o)==x])))
       out[i] <- list(o)
     }
@@ -53,18 +74,12 @@ raster2random <- function(in.data = NULL, na.rm = FALSE){
   # convert to dataframe
   out <- as.data.frame(out)
   
-  if(na.rm == TRUE){
-    # filter out rows where the first value column is NA
-    out <- dplyr::filter(out, !is.na(out[,1]))
-  }
+  ## assign rownames to dataframe
+  ### pixel X year
+  #rownames(out) <- paste0(out$pix, out$time)
   
-  
-  # assign rownames to dataframe
-  ## pixel X year
-  rownames(out) <- paste0(out$pix, out$time)
-  
-  # remove pix and time columns
-  out <- out[,3:ncol(out)]
+  ## remove pix and time columns
+  #out <- out[,3:ncol(out)]
   
   # return result
   return(out)
---
+++ raster2random_s2.r
@@ -0,0 +1,71 @@
+#' raster2random - Stage 2
+#' 
+#' Takes a named list of raster bricks and converts it to a long-format 
+#' dataframe for use with the randomForest function
+#' 
+#' Stage 2: Merges dependent variable with desired independent columns
+#' 
+#' @param in.dep DEPENDENT varia ble data. Provide a raster brick.
+#' @param dep.name Vector for naming the dependent variable column. Defaults to "avhrrp".
+#' @param in.ind INDEPENDENT variable data. Provide output from raster2random_s1. 
+#' Can be a single dataframe or a named list of dataframes.
+#' #@param months Vector of months from independent variable data to be included. 
+#' #To specify different months for different variables provide a list of 
+#' vectors of the same length as in.ind.
+#' @param na.rm Should instances where DEPENDENT = NA be removed?
+#' @param no.drift Have drift years been removed from in.dep data? T/F
+
+raster2random_s2 <- function(in.dep = NULL, dep.name = "avhrrp",
+                             in.ind = NULL, #months = 1:12, 
+                             na.rm = TRUE, no.drift = TRUE){
+  
+  # ensure that INDEPENDENT data is formatted as a list
+  if(class(in.ind) != "list") { in.ind <- list(independent = in.ind) }
+  
+  ## ensure that months are formatted as a list
+  #if(class(months) != "list") { months <- list(independent = months)}
+  
+  ## get data for given months from given INDEPENDENT dataframes
+  #ind <- list()
+  #for(i in 1:length(months)){
+  #  for(j in 1:length(in.ind)){
+  #    ind[[j]] <- in.ind[[j]][,months[[i]]]
+  #  }
+  #}
+
+  # convert selected INDEPENDENT data to a dataframe
+  #ind <- as.data.frame(ind, stringsAsFactors = FALSE)
+  ind <- as.data.frame(in.ind, stringsAsFactors = FALSE)
+  
+  # prepare dependent data
+  dep <- raster::as.data.frame(in.dep)
+  dates <- seq(from = 1989, to = 2014, by = 1)
+  if(no.drift == TRUE){
+    nd_p_band <- c(1:3, 7:10, 13:26)
+    dates <- dates[nd_p_band]
+  }
+  colnames(dep) <- dates
+  dep <- data.frame(pix = 1:nrow(dep), dep)
+  dep <- reshape2::melt(dep, id = "pix")
+  colnames(dep) <- c("pix", "year", dep.name)
+  
+  # join DEPENDENT and INDEPENDENT data
+  out <- cbind(dep, ind, stringsAsFactors = FALSE)
+  
+  # assign row names
+  rownames(out) <- paste0(out$pix, out$year)
+  
+  # remove pix and year columns
+  out <- out[,3:ncol(out)]
+  
+  if(na.rm == TRUE){
+    # filter out rows where the first value column is NA
+    out <- tibble::rownames_to_column(out)
+    out <- dplyr::filter(out, !is.na(out[,2]))
+    out <- tibble::column_to_rownames(out)
+  }
+  
+  # return result
+  return(out)
+  
+}
