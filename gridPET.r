7c757a55a8ad6392ac3289ecdb6a3a6d796d9753
Mike Stefanuk <mike.stefanuk@gmail.com>
2018-09-13 11:35:28 -0400

    option to do separate PET calculation

---
+++ gridPET.r
@@ -0,0 +1,61 @@
+#' gridPET
+#' 
+#' Calculates pixel-wise PET values for Daymet data
+#' 
+#' Uses for loops and pixel indexing
+#' 
+#' @param tmean File path or raster object. Monthly average temperature.
+#' @param start.year Numeric. Default = 1988.
+#' @param end.year Numeric. Default = 2014.
+#' @param na.rm Logical. Passed to SPEI package functions: thornthwaite
+
+gridPET <- function(tmean = NULL,
+                    start.year = 1988, end.year = 2014,
+                    na.rm = TRUE){
+  # input check
+  if(is.null(tmean) | is.null(prcp)){
+    stop('Please include tmean and prcp data')
+  }
+  if(class(start.year) != "numeric" | class(end.year) != "numeric"){
+    stop('start.year and end.year should be numeric')
+  }
+  
+  # load data
+  if(class(tmean) != "RasterBrick"){
+    tmean <- raster::brick(tmean, values = TRUE)
+  }
+  
+  # dates
+  dates <- seq(as.Date(sprintf("%s-01-15", start.year)),
+               as.Date(sprintf("%s-12-15", end.year)),
+               by = "month")
+  tmean <- raster::setZ(tmean, dates, name = "time")
+  names(tmean) <- zoo::as.yearmon(raster::getZ(tmean))
+  
+  # latitude
+  ref <- raster::projectExtent(tmean, "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+  int <- raster::projectRaster(from = tmean, to = ref)
+  latitude <- raster::setValues(tmean[[1]], sp::coordinates(int)[,"y"])
+  
+  # potential evapotranspiration
+  pet <- raster::brick(tmean, values = FALSE)
+  cat("calculating PET...\n")
+  pb1 = utils::txtProgressBar(min = 0, max = raster::ncell(tmean),
+                              initial = 0, style = 3)
+  for(i in 1:raster::ncell(tmean)){
+    pet_i <- SPEI::thornthwaite(Tave = as.vector(tmean[i]),
+                                lat = latitude[i],
+                                na.rm = na.rm)
+    pet[i] <- as.vector(pet_i)
+    utils::setTxtProgressBar(pb1,i)
+  }
+  
+  # format output
+  pet <- raster::setZ(pet, dates, name = "time")
+  names(pet) <- zoo::as.yearmon(raster::getZ(tmean))
+  
+  # return result
+  cat("\nc'est fini...")
+  return(pet)
+  
+}
--- gridSPEI.r
+++ gridSPEI.r
@@ -7,6 +7,7 @@
 #' 
 #' @param tmean File path or raster object. Monthly average temperature.
 #' @param prcp File path or raster object. Monthly total precipitation.
+#' @param pet File path or raster object. Monthly potential evapotranspiration.
 #' @param scale Numeric. At which scale should SPEI be calculated 
 #' (number of months back from 1, which is current month)?
 #' @param start.year Numeric. Default = 1988.
@@ -14,7 +15,7 @@
 #' @param na.rm Logical. Passed to SPEI package functions: thornthwaite & spei.
 #' @param pet.out Logical. Should PET be included in the output?
 
-gridSPEI <- function(tmean = NULL, prcp = NULL, 
+gridSPEI <- function(tmean = NULL, prcp = NULL, pet = NULL, 
                      scale = 1, 
                      start.year = 1988, end.year = 2014, 
                      na.rm = TRUE,
@@ -29,10 +30,22 @@ gridSPEI <- function(tmean = NULL, prcp = NULL,
   if(class(scale) != "numeric"){
     stop('scale should be numeric')
   }
+  if(is.null(pet)){
+    cat('calculating PET and SPEI')
+  }
   
   # load data
-  if(class(tmean) != "RasterBrick"){tmean <- raster::brick(tmean, values = TRUE)}
-  if(class(prcp)  != "RasterBrick"){prcp  <- raster::brick(prcp,  values = TRUE)}
+  if(class(tmean) != "RasterBrick"){
+    tmean <- raster::brick(tmean, values = TRUE)
+  }
+  if(class(prcp)  != "RasterBrick"){
+    prcp <- raster::brick(prcp, values = TRUE)
+  }
+  if(!is.null(pet)){
+    if(class(pet)  != "RasterBrick"){
+    pet <- raster::brick(pet, values = TRUE)
+    }
+  }
   
   # dates
   dates <- seq(as.Date(sprintf("%s-01-15", start.year)),
@@ -44,23 +57,26 @@ gridSPEI <- function(tmean = NULL, prcp = NULL,
   names(prcp) <- zoo::as.yearmon(raster::getZ(prcp))
   
   # latitude
-  ref <- raster::projectExtent(tmean, "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
-  int <- raster::projectRaster(from = tmean, to = ref)
-  latitude <- raster::setValues(tmean[[1]], sp::coordinates(int)[,"y"])
-  
-  # potential evapotranspiration
-  pet <- raster::brick(tmean, values = FALSE)
-  cat("calculating PET...\n")
-  pb1 = utils::txtProgressBar(min = 0, max = raster::ncell(tmean),
-                              initial = 0, style = 3)
-  for(i in 1:raster::ncell(tmean)){
-    pet_i <- SPEI::thornthwaite(Tave = as.vector(tmean[i]),
-                                 lat = latitude[i],
-                                 na.rm = na.rm)
-    pet[i] <- as.vector(pet_i)
-    utils::setTxtProgressBar(pb1,i)
+  if(!is.null(pet)){
+    ref <- raster::projectExtent(tmean, "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+    int <- raster::projectRaster(from = tmean, to = ref)
+    latitude <- raster::setValues(tmean[[1]], sp::coordinates(int)[,"y"])
   }
   
+  # potential evapotranspiration
+  if(is.null(pet)){
+    pet <- raster::brick(tmean, values = FALSE)
+    cat("calculating PET...\n")
+    pb1 = utils::txtProgressBar(min = 0, max = raster::ncell(tmean),
+                                initial = 0, style = 3)
+    for(i in 1:raster::ncell(tmean)){
+      pet_i <- SPEI::thornthwaite(Tave = as.vector(tmean[i]),
+                                  lat = latitude[i],
+                                  na.rm = na.rm)
+      pet[i] <- as.vector(pet_i)
+      utils::setTxtProgressBar(pb1,i)
+    }
+  }
   # spei
   spei <- raster::brick(tmean, values = FALSE)
   cat("\ncalculating SPEI...\n")
@@ -70,15 +86,13 @@ gridSPEI <- function(tmean = NULL, prcp = NULL,
     spei_i <- SPEI::spei(data = (as.vector(pet[i]) - as.vector(prcp[i])),
                           scale = scale,
                           na.rm = na.rm)
-    spei[i] <- as.vector(spei_i[[2]])
+    spei[i] <- as.vector(spei_i$fitted)
     utils::setTxtProgressBar(pb2,i)
   }
   
   # creating output
   if(pet.out == TRUE){
-    names(pet) <- sprintf("PET.%s",names(tmean))
-    names(spei) <- sprintf("SPEI.%s",names(tmean))
-    result <- raster::stack(pet, spei)
+    result <- list(spei = spei, pet = pet)
   } else {
     result <- raster::setZ(spei, dates, name = "time")
     names(result) <- zoo::as.yearmon(raster::getZ(tmean))
